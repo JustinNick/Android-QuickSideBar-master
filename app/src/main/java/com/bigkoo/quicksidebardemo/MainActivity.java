e07c6723f7790f64b205f4e8c4943b7f4894080a
JustinNick <2441178626@qq.com>
2016-12-10 12:42:12 +0800

    Initial commit

---
+++ app/src/main/java/com/bigkoo/quicksidebardemo/MainActivity.java
@@ -0,0 +1,189 @@
+package com.bigkoo.quicksidebardemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bigkoo.quicksidebar.QuickSideBarTipsView;
+import com.bigkoo.quicksidebar.QuickSideBarView;
+import com.bigkoo.quicksidebar.listener.OnQuickSideBarTouchListener;
+import com.bigkoo.quicksidebardemo.constants.DataConstants;
+import com.bigkoo.quicksidebardemo.model.City;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
+
+import java.lang.reflect.Type;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity implements OnQuickSideBarTouchListener {
+
+    RecyclerView recyclerView;
+    HashMap<String, Integer> letters = new HashMap<>();
+    QuickSideBarView quickSideBarView;
+    QuickSideBarTipsView quickSideBarTipsView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        quickSideBarView = (QuickSideBarView) findViewById(R.id.quickSideBarView);
+        quickSideBarTipsView = (QuickSideBarTipsView) findViewById(R.id.quickSideBarTipsView);
+
+        //设置监听
+        quickSideBarView.setOnQuickSideBarTouchListener(this);
+
+
+        //设置列表数据和浮动header
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Add the sticky headers decoration
+        CityListWithHeadersAdapter adapter = new CityListWithHeadersAdapter();
+
+        //Gson解析出来
+        Type listType = new TypeToken<LinkedList<City>>() {
+
+        }.getType();
+        Gson gson = new Gson();
+        final LinkedList<City> cities = gson.fromJson(DataConstants.cityDataList, listType);
+
+        ArrayList<String> customLetters = new ArrayList<>();
+
+        int position = 0;
+        for (City city : cities) {
+            String letter = city.getFirstLetter();
+            //如果没有这个key则加入并把位置也加入
+            if (!letters.containsKey(letter)) {
+                letters.put(letter, position);
+                customLetters.add(letter);
+            }
+            position++;
+        }
+
+        //不自定义则默认26个字母
+        quickSideBarView.setLetters(customLetters);
+        adapter.addAll(cities);
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new MyItemClickListener() {
+
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this,"点击了"+cities.get(position).getCityName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final StickyRecyclerHeadersDecoration headersDecor = new StickyRecyclerHeadersDecoration(adapter);
+        recyclerView.addItemDecoration(headersDecor);
+
+        // Add decoration for dividers between list items
+        recyclerView.addItemDecoration(new DividerDecoration(this));
+    }
+
+
+    @Override
+    public void onLetterChanged(String letter, int position, float y) {
+        quickSideBarTipsView.setText(letter, position, y);
+        //有此key则获取位置并滚动到该位置
+        if (letters.containsKey(letter)) {
+            recyclerView.scrollToPosition(letters.get(letter));
+        }
+    }
+
+    @Override
+    public void onLetterTouching(boolean touching) {
+        //可以自己加入动画效果渐显渐隐
+        quickSideBarTipsView.setVisibility(touching ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    private class CityListWithHeadersAdapter extends CityListAdapter<MyViewHolder>
+            implements StickyRecyclerHeadersAdapter<MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_item, parent, false);
+            return new MyViewHolder(view,onItemClickListener) {
+
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(getItem(position).getCityName());
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            return getItem(position).getFirstLetter().charAt(0);
+        }
+
+        @Override
+        public MyViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.view_header, parent, false);
+            return new MyViewHolder(view,onItemClickListener) {
+
+            };
+        }
+
+        private MyItemClickListener onItemClickListener;
+
+        public void setOnItemClickListener(MyItemClickListener onItemClickListener) {
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(MyViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            textView.setText(String.valueOf(getItem(position).getFirstLetter()));
+            holder.itemView.setBackgroundColor(getRandomColor());
+
+        }
+
+        private int getRandomColor() {
+            SecureRandom rgen = new SecureRandom();
+            return Color.HSVToColor(150, new float[]{
+                    rgen.nextInt(359), 1, 1
+            });
+        }
+
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private MyItemClickListener myItemClickListener;
+
+        public MyViewHolder(View itemView, MyItemClickListener myItemClickListener) {
+            super(itemView);
+            this.myItemClickListener = myItemClickListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (myItemClickListener != null) {
+                myItemClickListener.onItemClick(v, getPosition());
+            }
+        }
+    }
+
+    public interface MyItemClickListener {
+
+        void onItemClick(View view, int position);
+    }
+}
